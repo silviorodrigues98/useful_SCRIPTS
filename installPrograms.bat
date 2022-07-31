25a58457efc6ac49f9c999aaad54072c76e8689c
Silvio Rodrigues Correa <silviorodrigues98@hotmail.com>
2022-07-31 20:48:52 -0300

    Create installPrograms.bat

---
+++ installPrograms.bat
@@ -0,0 +1,50 @@
+@echo off
+cls
+@REM COLOCAR AQUI O CAMINHO COMPLETO ATÉ A POSTA ONDE ESTÃO OS .BAT OU .MSI
+set pathToFiles="C:\Users\Silvio\Downloads\testingBATCH\x64\"
+@REM //TODO ADD FUNCTIONALITY TO RECOGNIZE SYSTEM`S ARCHITECTURE
+@REM THE IDEA IS TO HAVE TWO FOLDERS FOR WINDOWS, ONE 32 AND ONE 64 BITS.
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+echo. 
+echo.
+timeout 1 >nul
+echo SISTEMA DE %OS%, ARQUIVOS CONTIDOS NA PASTA %pathToFiles%.
+echo CONFIRA SE EXECUTOU EM MODO ADMINISTRADOR E SE AS INFORMACOES ACIMA ESTAO CERTAS.
+echo A INSTALACAO PODE DEMORAR UM POUCO, AGUARDE ATE O FINAL DA MESMA, 
+echo E NAO FACA OPERACOES QUE SOBRECARREGUEM O SISTEMA.
+pause
+echo. 
+echo.
+echo INICIANDO INSTALACAO DE ARQUIVOS EXE...
+echo. 
+echo.
+timeout 3 >nul
+setlocal DisableDelayedExpansion
+@REM THIS WILL LOOP ALL .EXE FILES INSIDE THE FOLDER AND INSTALL THEM
+for %%I in (%pathToFiles%"*.exe") do (
+    echo INSTALANDO: %%~nI
+    timeout 2 >nul
+    start "Running %%~nI" /wait "%%I" /S
+    echo.
+    echo.
+)
+echo INICIANDO INSTALACAO DE ARQUIVOS MSI...
+echo. 
+echo.
+timeout 3 >nul
+@REM THIS WILL LOOP ALL .MSI FILES INSIDE THE FOLDER AND INSTLAL THEM
+for %%I in (%pathToFiles%"*.msi") do (
+    echo INSTALANDO: %%~nI
+    timeout 2 >nul
+    msiexec /i %%I /quiet /norestart 
+    echo.
+    echo.
+)
+endlocal
+echo. 
+echo.
+@REM //TODO FIND A WAY TO VERIFY IF PROGRAMS WERE INSTALLED AUTOMATICALLY
+@REM THIS WILL OPEN PROGRAMS MANAGER, SO THAT YOU CAN SEE IF THE PROGRAM WAS PROPERLY INSTALLED
+appwiz.cpl
+echo INSTALACAO FINALIZADA, CONFIRA SE OS PROGRAMAS FORAM INSTALADOS CORRETAMENTE.
+pause
