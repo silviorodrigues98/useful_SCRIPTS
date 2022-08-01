7f836b000a13b33cc04a6ede66658df1a6aba11f
Silvio Rodrigues Correa <silviorodrigues98@hotmail.com>
2022-07-31 21:53:25 -0300

    Update installPrograms.bat

--- installPrograms.bat
+++ installPrograms.bat
@@ -1,10 +1,18 @@
 @echo off
 cls
 @REM COLOCAR AQUI O CAMINHO COMPLETO ATÉ A POSTA ONDE ESTÃO OS .BAT OU .MSI
-set pathToFiles="C:\Users\Silvio\Downloads\testingBATCH\x64\"
-@REM //TODO ADD FUNCTIONALITY TO RECOGNIZE SYSTEM`S ARCHITECTURE
+set pathToFiles=C:\Users\Silvio\Downloads\testingBATCH\
 @REM THE IDEA IS TO HAVE TWO FOLDERS FOR WINDOWS, ONE 32 AND ONE 64 BITS.
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==32BIT set pathToFiles=%pathToFiles%x86\
+if %OS%==64BIT set pathToFiles=%pathToFiles%x64\
+@REM CHECKS IF FOLDER EXISTS, AND IF IT DOESN'T, IT BREAKS THE PROGRAM
+if not exist "%pathToFiles%" (
+    echo O DIRETORIO ESPECIFICADO NO .BAT NAO FOI ENCONTRADO, FAVOR ALTERAR A CONFIGURACAO.
+    echo PRESSIONE QUALQUER TELA PARA FECHAR ESTA JANELA
+    pause
+    exit
+)
 echo. 
 echo.
 timeout 1 >nul
@@ -21,7 +29,7 @@ echo.
 timeout 3 >nul
 setlocal DisableDelayedExpansion
 @REM THIS WILL LOOP ALL .EXE FILES INSIDE THE FOLDER AND INSTALL THEM
-for %%I in (%pathToFiles%"*.exe") do (
+for %%I in ("%pathToFiles%*.exe") do (
     echo INSTALANDO: %%~nI
     timeout 2 >nul
     start "Running %%~nI" /wait "%%I" /S
@@ -33,7 +41,7 @@ echo.
 echo.
 timeout 3 >nul
 @REM THIS WILL LOOP ALL .MSI FILES INSIDE THE FOLDER AND INSTLAL THEM
-for %%I in (%pathToFiles%"*.msi") do (
+for %%I in ("%pathToFiles%*.msi") do (
     echo INSTALANDO: %%~nI
     timeout 2 >nul
     msiexec /i %%I /quiet /norestart 
