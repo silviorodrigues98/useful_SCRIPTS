9d8c202c2f30f9ef882bc23f759492a24cc4b425
Silvio Correa <silviorodrigues98@hotmail.com>
2023-07-23 15:36:57 -0300

    policies bat

---
+++ reset_policies.bat
@@ -0,0 +1,20 @@
+@echo off
+
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKCU\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKCU\Software\Policies" /f
+reg delete "HKLM\Software\Microsoft\Policies" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate" /f
+reg delete "HKLM\Software\Microsoft\WindowsSelfHost" /f
+reg delete "HKLM\Software\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Policies" /f
+reg delete "HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate" /f
+
+RD /S /Q "%WinDir%\System32\GroupPolicy"
+RD /S /Q "%WinDir%\System32\GroupPolicyUsers"
+
+gpupdate /force
+
+pause
