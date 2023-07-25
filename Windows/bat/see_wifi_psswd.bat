c01320979258fb9d9eb468096c26dd58c92205b5
Silvio Correa <silviorodrigues98@hotmail.com>
2023-07-24 23:35:43 -0300

    wifi passwords

--- Windows/bat/see_wifi_psswd.bat
+++ Windows/bat/see_wifi_psswd.bat
@@ -1,22 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
-
-:menu
-cls
-set i=1
-for /f "skip=9 tokens=4,* delims=: " %%a in ('netsh wlan show profiles') do (
-    set profile[!i!]=%%a
-    echo !i!. %%a
-    set /a i+=1
+for /f "skip=9 tokens=1,* delims=:" %%a in ('netsh wlan show profiles') do (
+    set "profile=%%b"
+    set "profile=!profile:~1!"
+    netsh wlan show profile name="!profile!" key=clear | findstr "SSID Key Content"
 )
-echo.
-set /p choice=Enter the number of the network profile to display the password: 
-call :show_password "!profile[%choice%]!"
-pause
-goto menu
 
-:show_password
-set profile_name=%~1
-set profile_name=%profile_name:~1%
-netsh wlan show profile "%profile_name%" key=clear
-goto :eof
+pause
