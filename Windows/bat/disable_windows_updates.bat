e2a5f00010ab965a913be3c2893c720548d05d49
Silvio Correa <silviorodrigues98@hotmail.com>
2023-09-17 09:15:03 -0300

    refactored whole code

--- Windows/bat/disable_windows_updates.bat
+++ Windows/bat/disable_windows_updates.bat
@@ -1,28 +1,25 @@
-Here is the equivalent PowerShell script for the batch script you provided:
+# Specify the location to save the backup
+$backupPath = "C:\\RegistryBackup.reg"
 
-```powershell
+# Export the entire registry
+Write-Host "Creating a backup of your registry..."
+reg export HKLM $backupPath
+if ($?) {
+    Write-Host "Registry backup has been created successfully at $backupPath"
+} else {
+    Write-Host "Failed to create a registry backup."
+    exit
+}
+
 # Stop the Windows Update service
 Stop-Service -Name wuauserv
 
 # Set the startup type of the Windows Update service to disabled
 Set-Service -Name wuauserv -StartupType Disabled
 
-# Disable automatic updates in Windows 11
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
-$propertyName = "NoAutoUpdate"
-$propertyValue = 1
-
-# Create the registry path if it does not exist
-if (!(Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force | Out-Null
-}
-
-# Set the property value
-Set-ItemProperty -Path $registryPath -Name $propertyName -Value $propertyValue
-```
-
-# Disable automatic updates in Windows
-$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+# Disable automatic updates via Local Group Policy Editor
+Write-Host "Disabling automatic updates via Local Group Policy Editor..."
+$registryPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Group Policy Objects\{48981759-12F2-42A6-A048-028B3973495F}Machine\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"
 $propertyName = "NoAutoUpdate"
 $propertyValue = 1
 
@@ -34,3 +31,16 @@ if (!(Test-Path $registryPath)) {
 # Set the property value
 Set-ItemProperty -Path $registryPath -Name $propertyName -Value $propertyValue
 
+# Disable automatic updates via Registry
+Write-Host "Disabling automatic updates via Registry..."
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"
+$propertyName = "AUOptions"
+$propertyValue = 1
+
+# Create the registry path if it does not exist
+if (!(Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+# Set the property value
+Set-ItemProperty -Path $registryPath -Name $propertyName -Value $propertyValue
