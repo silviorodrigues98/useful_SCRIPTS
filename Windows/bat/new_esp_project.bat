6489e6802c90a694ccef2a74f1e0977d55ef04a5
Silvio Rodrigues Correa <silviorodrigues98@hotmail.com>
2023-11-12 09:21:11 -0300

    now it opens vscode when finished

--- Windows/bat/new_esp_project.bat
+++ Windows/bat/new_esp_project.bat
@@ -1,6 +1,8 @@
 @echo off
-set folder_name=esp_TEMPLATE
+set folder_name=folder_to_copy
 set /p new_folder_name=Enter the name of the new folder:
 set script_dir=%~dp0
 xcopy /e /i "%script_dir%%folder_name%" "%script_dir%%new_folder_name%"
 rd /s /q "%script_dir%%new_folder_name%\.git"
+cd "%script_dir%%new_folder_name%"
+code .
